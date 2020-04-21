5e4b76ab405522e05a74f1ad4096c9f246c609e2
mehmetadil <adil.istikbal@fazlagida.com>
2020-04-21 13:45:37 +0300

    question scaffold generated. user-question relation defined. root page edited.

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_04_21_102257) do
+ActiveRecord::Schema.define(version: 2020_04_21_103952) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "questions", force: :cascade do |t|
+    t.string "title"
+    t.text "description"
+    t.integer "user_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
